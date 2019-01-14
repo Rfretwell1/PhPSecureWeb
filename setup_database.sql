5181d136516d27cbda8007567e3d08b83214ea34
Josh Davis <p16166207@my365.dmu.ac.uk>
2019-01-14 02:02:10 +0000

    Changed sql_commands.txt to a .sql script

--- setup_database.sql
+++ setup_database.sql
@@ -1,11 +1,9 @@
-//THESE COMMANDS SHOULD ONLY HAVE TO BE RAN ONCE PER MACHINE
-
-//Creates the database & user
+/*Creates the database & user*/
 	CREATE DATABASE coursework_db COLLATE utf8_unicode_ci;
 
 	GRANT SELECT, INSERT, UPDATE on coursework_db.* TO 'cw_user'@'localhost' IDENTIFIED BY 'cw_password';
 
-//Creates the table to hold the message info
+/*Creates the table to hold the message info*/
 	USE coursework_db;
 	DROP TABLE IF EXISTS messages;
 	CREATE TABLE messages (
@@ -20,7 +18,7 @@
 		CONSTRAINT msg_pk PRIMARY KEY (msg_keypad, msg_timestamp, msg_switch4)
 	) ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci; 
 
-//Creates the table to hold account info
+/*Creates the table to hold account info*/
 	USE coursework_db;
 	DROP TABLE IF EXISTS accounts;
 	CREATE TABLE accounts (
